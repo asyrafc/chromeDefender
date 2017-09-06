f7b6dfb76d5fdf8f493ad6bf695f09a9d1d6ba3d
ghostbyte <asyrafcloud@gmail.com>
2017-09-06 18:02:34 +0800

    Updated cookies

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 //if(window.location.href.indexOf("git") > -1 || window.location.href.indexOf("how") > -1 )
 //{
-	// this line will render the html code in the website useless as everything will be commented out
+	//this line will render the html code in the website useless as everything will be commented out
 	//var newWindow = window.open();
 	//newWindow.document.write(document.documentElement.innerHTML);
 	//var i;
@@ -18,20 +18,23 @@
 	}
 	alert(arrayMaliciousContent.length);*/
 	
-	for(var i = 0; i < document.getElementsByTagName("*").length; i++)
-	{
-		alert(document.getElementsByTagName("*")[i]);
-	}
+	//for(var i = 0; i < document.getElementsByTagName("*").length; i++)
+	//{
+		//alert(document.getElementsByTagName("*")[i]);
+	//}
 	
-	/*document.write('<!--');
+	var cookie = document.cookie;
+
+	document.write('<!--');
 	document.write('-->');
 	// This line will overwrite the html code/content
 	document.write("<h1>Test</h1>");
 	document.write("<p>Page loading have been intercepted as the website that you visited just now might contain malicious content</p>");
-	for(var j = 0; j < arrayMaliciousContent.length; j++)
-	{
-		document.write(arrayMaliciousContent[j]);
-	}*/
+	document.write("<p>Cookie is : " + cookie + "</p>");
+	//for(var j = 0; j < arrayMaliciousContent.length; j++)
+	//{
+		//document.write(arrayMaliciousContent[j]);
+	//}
 	
 	//newWindow.document.write("<p>You have been redirected as the website that you visited just now might contain malicious content</p>");
 	
